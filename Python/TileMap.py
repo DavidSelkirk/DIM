aefd7c824c3a601570c5fcc7f8dbd1288ec5023b
David Selkirk <david_selkirk@hotmail.co.uk>
2013-03-03 22:51:46 +0000

    Bug fixes, walls tested

--- Python/TileMap.py
+++ Python/TileMap.py
@@ -5,26 +5,41 @@ import MapEntity
 #rowSize = 0
 #columnSize = 0
 
+
+
 class TileMap:
         def __init__(self, filename):
+                #setup variables
                 self.tiles = []
                 backgroundTiles = []
+                self.legalTiles = []
+                self.initLegalTiles()
                 f = open(filename)
                 #read the size of the map
                 self.rowSize, self.columnSize = [int(x) for x in f.readline().split()]
+                print self.rowSize, self.columnSize
                 #read each tile symbol from file
                 for i in range(0, self.rowSize):
                         x = []
                         for j in range(0, self.columnSize):
                                 c = f.read(1)
-                                if c != " " and c != "\n":
-                                        x.append(c)
+                                #print "Read " + str(c)
+                                while c not in self.legalTiles:
+                                        c = ""
+                                        c = f.read(1)
+                                x.append(c)
                         self.tiles.append(x)
-                self.backgroundTiles = self.copy(self.tiles)
-                for i in range(0, self.rowSize):
-                        print self.backgroundTiles[i]
-                self.printMap()
+                        #print "Next row"
+                        #print
 
+
+                self.backgroundTiles = self.copy(self.tiles)
+
+        def initLegalTiles(self): #base background tiles
+                self.legalTiles.append("W")
+                self.legalTiles.append("X")
+ 
+                        
         def copy(self, source):
                 temp = []
                 for i in range(0, self.rowSize):
@@ -39,6 +54,7 @@ class TileMap:
                 #set player position
                 self.tiles = self.copy(self.backgroundTiles)
                 self.tiles[p.xPos][p.yPos] = p.mapImage
+                self.printMap()
                 #self.printMap()
 
         #print the game map
@@ -52,13 +68,19 @@ class TileMap:
                 if not direction in moves:
                         print "Not a legal move"
                         return
+                #up
                 if(direction == "up" and self.tiles[p.xPos-1][p.yPos] != "W"):
                         p.xPos -= 1
+                #down
                 if(direction == "down" and self.tiles[p.xPos+1][p.yPos] != "W"):
                         p.xPos += 1
-                        print "Here"
+                #left
+                if(direction == "left" and self.tiles[p.xPos][p.yPos-1] != "W"):
+                        p.yPos -= 1
+                #right
+                if(direction == "right" and self.tiles[p.xPos][p.yPos+1] != "W"):
+                        p.yPos += 1
                 self.populateMap(p)
-                self.printMap()
 
         def getPlayerMoves(self, p):
                 moveList = []
@@ -68,4 +90,12 @@ class TileMap:
                 #down
                 if(p.xPos + 1 < self.rowSize and self.tiles[p.xPos+1][p.yPos] != "W"):
                         moveList.append("down")
+                #left
+                if(p.yPos - 1 >= 0 and self.tiles[p.xPos][p.yPos-1] != "W"):
+                        moveList.append("left")
+                #right
+                print str(p.yPos + 1) + " " + str(self.columnSize)
+                if(p.yPos + 1 < self.columnSize and self.tiles[p.xPos][p.yPos+1] != "W"):
+                        moveList.append("right")
+                        
                 return moveList
