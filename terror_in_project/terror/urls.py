39925cc3dd6fd45cb486c8ff429d7e881a860f48
David Selkirk <1003646s@bo720-5-05.(none)>
2013-03-05 14:07:05 +0000

    Added base game page

--- terror_in_project/terror/urls.py
+++ terror_in_project/terror/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from terror import views
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index')
+        url(r'^$', views.index, name='index'),
+	url(r'^game$', views.game, name='game')
 )
