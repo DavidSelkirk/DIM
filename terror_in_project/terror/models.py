970383c9ce867eedb5f1bbaa58445fe1c6a31257
David Selkirk <1003646s@bo720-4-04.(none)>
2013-03-08 16:54:29 +0000

    Basic single player single session working

--- terror_in_project/terror/models.py
+++ terror_in_project/terror/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django import forms
 # Create your models here.
 
-class GameForm(forms.ModelForm):
-        name = forms.CharField(max_length=50,
+class GameForm(forms.Form):
+        move = forms.CharField(max_length=10,
                 help_text='Please enter your move.')
 
--- terror_in_project/terror/views.py
+++ terror_in_project/terror/views.py
@@ -1,8 +1,14 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.shortcuts import render_to_response
+from models import GameForm
 from MapEntity import *
 import TileMap
+
+tilemap = TileMap.TileMap("/users/level3/1003646s/work/DIM3/DIM/terror_in_project/terror/Map.txt")
+player = Player(1)
+zombie = Zombie(1, 1, 3)
 
 def index(request):
         # select the appropriate template to use
@@ -21,11 +27,23 @@ def game(request):
         # but later on we will be putting data in the context which the template engine
         # will use when it renders the template into a page.
 	#create the TileMap
-	tilemap = TileMap.TileMap("/users/level3/1003646s/work/DIM3/DIM/terror_in_project/terror/Map.txt")
-	player = Player(1)
-	zombie = Zombie(1, 1, 3)
-	tilemap.populateMap(player, zombie)
+	if request.method == 'POST':
+		print "In POST"
+		form = GameForm(request.POST)
+		if form.is_valid():
+			direction = form.cleaned_data['move']
+			moves = tilemap.getPlayerMoves(player)
+			tilemap.movePlayer(player, zombie, moves, direction)
+			tilemap.populateMap(player, zombie)
+		tiles = tilemap.tiles
+		context = RequestContext(request, {'tiles' : tiles})
+		form = GameForm()
+		return render_to_response('terror/game.html', {'form': form }, context)
+	else:
+		tilemap.populateMap(player, zombie)
+		form = GameForm()
 	tiles = tilemap.tiles
         context = RequestContext(request, {'tiles' : tiles})
+		
         # render the template using the provided context and return as http response.
-        return HttpResponse(template.render(context))
+        return render_to_response('terror/game.html', {'form': form }, context)
